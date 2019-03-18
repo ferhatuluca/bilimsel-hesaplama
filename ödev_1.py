b63d039e518cd8f3f8e640a2fa348194c88b820e
Reber Ferhat Uluca <34395388+ferhatuluca@users.noreply.github.com>
2019-03-18 23:39:45 +0300

    Güncellendi

--- ödev_1.py
+++ ödev_1.py
@@ -1,5 +1,6 @@
 """ 2 x 2 'lik matrisler için """
-def denklem(liste):
+
+def determinand_two_by_two(liste):
     yeni = [[], []]
     for j in range(1, -1, -1):
         for i in range(len(liste[0])):
@@ -8,29 +9,26 @@ def denklem(liste):
             else:
                 yeni[j].append(liste[j][i] - liste[0][1]/yeni[1][1] * yeni[1][i])
 
-    print("Y değişkeninin değeri : ", yeni[1][2]/yeni[1][1], "  X değikeninin değeri : ", yeni[0][2]/yeni[0][0])
+    print("2x2'lik matris\n", "Y değişkeninin değeri : ", yeni[1][2]/yeni[1][1], "  X değikeninin değeri : ", yeni[0][2]/yeni[0][0])
 
-
-liste = [[2, 3, 7], [4, 2, 8]]
-denklem(liste)
 
 """ N x N matrisler için """
 
-def denklem(liste):
+def determinand_N_by_N(liste):
     list = liste.copy()
     satir = len(liste)
     sütun = len(liste[0])
-    
-    for k in range(satir-1):
-        for i in range(satir-1-k):
+
+    for k in range(satir-1):                            # Satirları dolaşan döngü.
+        for i in range(satir-1-k):                      # Sütunları sıfır yapılacak en son değişkene kadar dolaşan döngü.
             carpan = list[i][k]/list[i+1][k]
-            for j in range(sütun):
+            for j in range(sütun):                      # Sütunları çarpan ile çarpmak için dolaşan döngü.
                 list[i][j] += -carpan * list[i+1][j]
 
 
-    print("sol üçgen matris", list, "\n")
-    
-    count = 0                                   # Matriste en alt satırdan üst satırlara gidilmesi için gereken değişken.
+    print("\nsol üçgen matris", list)
+
+    count = 0                                           # Matriste en alt satırdan üst satırlara gidilmesi için gereken değişken.
     for p in range(satir-1, 0, -1):
         for t in range(satir-1, count, -1):
             carpan = list[t][p]/list[t-1][p]
@@ -38,13 +36,12 @@ def denklem(liste):
                 list[t][h] += -carpan * list[t-1][h]
 
         count += 1
-        
+
     print("köşegen matris", list, "\n")
 
     for o in range(satir-1, -1, -1):
             print(satir-o, ". değişken değeri =", liste[o][satir]/liste[o][satir-o-1])
 
 
-liste = [[1, 2, 7, 4, 2, 4], [2, 5, 6, 7, 1, 3], [7, 8, 9, 10, 2, 12], [8, 1, 4, 3, 2, 21], [3, 5, 7, 2, 3, 2]]
-denklem(liste)
-
+liste_2 = [[2, 3, 5], [4, 8, 20]]
+determinand_N_by_N(liste_2)
